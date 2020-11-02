5551f08955a3ce20ebf5031e108802f6c19ff210
Keith Carlton <kcarlton@ruegiltgroupe.com>
2020-11-02 16:40:47 -0600

    Add new util for dealing with select elements and update README with new variable

---
+++ utils/select-utils.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const assert = require('assert');
+const { By } = require('selenium-webdriver')
+
+module.exports.selectByValue = async (selectElem, valueDesired) => {
+  selectElem.findElements(By.css('option'))
+    .then(options => {
+      options.map(option => {
+        option.getAttribute('value')
+          .then(value => {
+            if (value === valueDesired) {
+              option.click();
+            }
+          });
+      });
+    });
+}
+
+module.exports.assertValueIsSelected = async (selectElem, expectedValue) => {
+  let foundSelectedValue = undefined;
+
+
+  const options = await selectElem.findElements(By.css('option'));
+  for (let i = 0; i < options.length; i++) {
+    const option = options[i];
+    const selected = await option.isSelected();
+    if (selected) {
+      foundSelectedValue = await option.getAttribute('value');
+    }
+  }
+  assert.strictEqual(foundSelectedValue, expectedValue);
+  //
+  //
+  //   .then(options => {
+  //     options.map(option => {
+  //       option.isSelected()
+  //         .then(selected => {
+  //           if(selected) {
+  //             option.getAttribute('value')
+  //               .then(value => foundSelectedValue = value);
+  //           }
+  //         });
+  //     });
+  //   });
+  // assert.strictEqual(foundSelectedValue, expectedValue);
+}
